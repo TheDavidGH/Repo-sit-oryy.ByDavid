3ec2f9c87592e8c6a4539f434f8b211817091d8e
TheDavidGH <lilopoes2002@gmail.com>
2021-11-22 20:52:53 +0100

    Fet el EX9 re chungo pa

--- Fitxer.js
+++ Fitxer.js
@@ -1,4 +1,3 @@
-//EX7
-Math.hypot = Math.hypot || function(x, y){ return Math.sqrt(x*x + y*y) } 
-var x = 4, y = 4; document.write(Math.hypot(x, y)) 
-//se printa en el Body el resultado :D
+//EX9
+var NC = 'DavidJorda'
+console.log(NC.substr(5,6,7,8,9) + " (En total son 5)");
