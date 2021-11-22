af64cd4de8d0be73064b5ff046e216ffbc3ecb1a
TheDavidGH <lilopoes2002@gmail.com>
2021-11-22 20:24:47 +0100

    Se printa en el body pero en fin que se le vamos hacer

--- Fitxer.js
+++ Fitxer.js
@@ -1,33 +1,4 @@
-//EX6
-function myRound(num, dec) {
-    var exp = Math.pow(10, dec || 1);
-    return parseInt(num * exp, 10) / exp;
-  }
-//suma
-function sum(a, b) {
-    return a+b;
-  }
-  let results = sum(1.234, 5.678); 
-  console.log('Multiplicar= ', myRound(results, 1));
-
-//resta
-  function resta(a, b) {
-    return a-b;
-  }
-  let resultr = resta(1.234, 5.678); 
-  console.log('Multiplicar= ', myRound(resultr, 1));
-
-//dividir
-function dividir(a, b) {
-    return a/b;
-  }
-  let resultd = dividir(1.234, 5.678); 
-  console.log('Multiplicar= ', myRound(resultd, 1));
-
-  //multiplicar
-  function Multiplicar(a, b) {
-    return a*b;
-  }
-  let resultm = Multiplicar(1.234, 5.678); 
-  console.log('Multiplicar= ', myRound(resultm, 1));
-  
+//EX7
+Math.hypot = Math.hypot || function(x, y){ return Math.sqrt(x*x + y*y) } 
+var x = 4, y = 4; document.write(Math.hypot(x, y)) 
+//se printa en el Body el resultado :D
