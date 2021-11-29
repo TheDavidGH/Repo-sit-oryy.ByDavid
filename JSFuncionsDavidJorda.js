d51ac2876e15785a11149091a2ca4438116a4cf9
TheDavidGH <lilopoes2002@gmail.com>
2021-11-29 21:19:07 +0100

    No se bien que queras decir peor aqui esta ex 7 :/

--- JSFuncionsDavidJorda.js
+++ JSFuncionsDavidJorda.js
@@ -1,8 +1,19 @@
+//EX 3
+    function funcionNumber(number = 1 ){
+        var f = new Date();
+        console.log(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
+    }
+    funcionNumber();
+
 //EX 6
-function suma(){
-    return firstPos,secondPos,therdPos;
-}
-    var firstPos = 3
-    var secondPos = 1
-    var therdPos = 2
-    console.log (firstPos+secondPos+therdPos)
+    function suma(){
+        return firstPos,secondPos,therdPos;
+    }
+        var firstPos = 3
+        var secondPos = 1
+        var therdPos = 2
+        console.log (firstPos+secondPos+therdPos)
+
+//EX 7
+    funcionNumber();
+    console.log(firstPos+secondPos+therdPos)
