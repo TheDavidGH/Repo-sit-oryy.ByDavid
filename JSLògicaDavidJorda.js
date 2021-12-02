b61ec8afa822f0dc8f28a7f27ae2a3c9db773f6d
TheDavidGH <lilopoes2002@gmail.com>
2021-12-02 20:47:01 +0100

    EX4

--- JSLògicaDavidJorda.js
+++ JSLògicaDavidJorda.js
@@ -1,20 +1,17 @@
-//EX3 
-var notaDelAlubne = 7
+//EX4
+var firstStr;
+firstStr=parseInt(prompt("posa numero: ")); //para que te salga el pop up
+sum =0;
+constante=1
+contador=0
+sumador2=1
+acumulado=0
 
-switch(true){
-case (notaDelAlubne<5):
-    console.log("SUSPENDISTE.")
-break
-case (notaDelAlubne>=5 && notaDelAlubne<6):
-    console.log("bien estas como yo.")
-break             
-case (notaDelAlubne>=6 && notaDelAlubne<7):
-    console.log("refachero sige asi.")
-break            
-case (notaDelAlubne>=7 && notaDelAlubne<9):
-    console.log("bua si te esfuerzas mas consigiras morirte.")
-break
-case (notaDelAlubne>=9 && notaDelAlubne<10):
-    console.log("la perfecion no existe.")
-break
+while(firstStr>=acumulado){
+    sum = sum + 1
+    contador = sumador2 + constante
+    constante=0
+    sumador2++
+    acumulado = contador
 }
+console.log(sum);
