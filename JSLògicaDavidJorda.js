d603b15e1ac89646d7a7704b6e9a39cad0360a66
TheDavidGH <lilopoes2002@gmail.com>
2021-12-02 20:29:48 +0100

    EX2 + 10 -50

--- JSLògicaDavidJorda.js
+++ JSLògicaDavidJorda.js
@@ -1,15 +1,11 @@
-//EX 1
- function num1(firstPos){
-    if (firstPos%3 === 0){
-        console.log("Es un multiple de 3")
-    }
-    if (firstPos%7 === 0){
-        console.log("Es un multiple de 7")
-    }
-    if ((firstPos%3 !== 0) && (firstPos%7 !==0 )){
-        console.log("El numero introduit no es ningun numero multiple de 3 & de 7")
-    }
- }
- num1(2);
- num1(3);
- num1(7);
+//EX2 
+var firstPos = 10
+var secondPOS =50
+
+firstPos=(firstPos>=10 && firstPos<=50) ? console.log("El numero esta en el rango")
+                                        : console.log("El numero no esta en el rango")
+
+
+
+secondPOS=(secondPOS>=10 && secondPOS<=50) ? console.log("El numero esta en el rango")
+                                           : console.log("El numero no esta en el rango")
