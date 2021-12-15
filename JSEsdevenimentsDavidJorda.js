f018b417a12fd596bd143708026c7274dd7b1594
TheDavidGH <lilopoes2002@gmail.com>
2021-12-15 23:28:14 +0100

    La teoria no la entiendo :(

--- JSEsdevenimentsDavidJorda.js
+++ JSEsdevenimentsDavidJorda.js
@@ -1,7 +1,15 @@
-//EX 3
-let presionAnterior= ' ';
+//EX 4            
 
-document.onkeydown = function(presionDeNow) {
-    presionAnterior = presionAnterior + presionDeNow.key;
-document.getElementById("keyCupId").innerHTML=presionAnterior;
+var boton_click = document.getElementById("clic");
+var boton_restart = document.getElementById("restart");
+var count = document.getElementById("contador");
+var contador = 0;
+
+function clic () {
+    contador = -1;
+}
+
+function Restart () {
+    contador++;
+    count.textContent = contador;
 }
